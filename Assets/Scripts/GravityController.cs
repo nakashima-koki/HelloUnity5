12f96a24042f0588d1d3fe82bee3231a826034f2
nakashima_koki <nakashima_koki@gmail.com>
2016-06-25 23:59:10 +0900

    Initial commit

---
+++ Assets/Scripts/GravityController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GravityController : MonoBehaviour {
+
+	const float Gravity = 9.81f;
+
+	public float gravityScale = 1.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 vector = new Vector3 ();
+
+		vector.x = Input.GetAxis("Horizontal");
+		vector.y = Input.GetAxis("Vertical");
+
+		if (Input.GetKey("z")) {
+			vector.y = 1.0f;
+		} else {
+			vector.y = -1.0f;
+		}
+		Physics.gravity = Gravity * vector.normalized * gravityScale;
+	}
+}
